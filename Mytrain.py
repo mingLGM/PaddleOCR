b5080d1d1d197e7df4a1b1ad31ee89a314592103
刘秩铭 <ming1096198165@163.com>
2025-09-05 17:18:01 +0800

    小修改

--- Mytrain.py
+++ Mytrain.py
@@ -33,8 +33,16 @@ python tools/export_model.py -c configs/det/PP-OCRv5/PP-OCRv5_mobile_det.yml -o 
 
 ###############################
 ####____gangban____####
-CUDA_VISIBLE_DEVICES=0 python -m paddle.distributed.launch --gpus '0' tools/train.py -c configs/det/PP-OCRv5/PP-OCRv5_mobile_det_gangban.yml 
+CUDA_VISIBLE_DEVICES=1 python -m paddle.distributed.launch --gpus '1' tools/train.py -c configs/det/PP-OCRv5/PP-OCRv5_mobile_det_gangban.yml
+CUDA_VISIBLE_DEVICES=0 python -m paddle.distributed.launch --gpus '0' tools/train.py -c configs/rec/PP-OCRv5/PP-OCRv5_mobile_rec_gangban.yml
 
 python tools/infer_det.py  -c configs/det/PP-OCRv5/PP-OCRv5_mobile_det_gangban.yml -o Global.infer_img="/home/zhangshuwen/work/datasets/gangban/paddleocr_dataset/det/images/00000_20250801_144146_1_CAM1_NG_Watermark1.bmp" Global.checkpoints="./output/gangban/PP-OCRv5_mobile_det/best_model/model"
 python tools/eval.py -c configs/det/PP-OCRv5/PP-OCRv5_mobile_det_gangban.yml -o Global.pretrained_model=./output/gangban/PP-OCRv5_mobile_det/best_model/model.pdparams Eval.dataset.data_dir=/home/zhangshuwen/work/datasets/gangban/paddleocr_dataset/det/ Eval.dataset.label_file_list=/home/zhangshuwen/work/datasets/gangban/paddleocr_dataset/det/det_label.txt
 python tools/infer/predict_system.py --image_dir="/home/zhangshuwen/work/datasets/gangban/paddleocr_dataset/det/images/" --det_model_dir="./output/gangban/PP-OCRv5_mobile_det/PP-OCRv5_mobile_det_infer/" --rec_model_dir="./output/gangban/PP-OCRv5_mobile_rec/PP-OCRv5_mobile_rec_infer/" --use_angle_cls=false
+
+
+python tools/infer_rec.py  -c configs/rec/PP-OCRv5/PP-OCRv5_mobile_rec_gangban.yml -o Global.infer_img="/home/zhangshuwen/work/datasets/gangban/paddleocr_dataset/rec/images/" Global.checkpoints="./output/gangban/PP-OCRv5_mobile_rec/latest"
+
+
+python tools/export_model.py -c configs/rec/PP-OCRv5/PP-OCRv5_mobile_rec_gangban.yml -o Global.pretrained_model="./output/gangban/PP-OCRv5_mobile_rec/best_model/model.pdparams" Global.save_inference_dir="./output/gangban/PP-OCRv5_mobile_rec/PP-OCRv5_mobile_rec_infer/"
+
